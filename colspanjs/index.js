bce3b6f592bc3657a904f4a9fe6349ce4e968e13
gomszab <gombosszabolcs88@gmail.com>
2025-12-16 19:12:47 +0100

    console col tablazat

---
+++ colspanjs/index.js
@@ -0,0 +1,6 @@
+console.log("Ókori település | Ágazat | Példa | |"); // Kiirom konzolra a tablazat fejlecet
+console.log("Athén | politika | demokrácia |  |") // Kiirom konzolra a tablazat 1. sorat
+console.log("Athén | tudomány | filozófia |  |") // Kiirom konzolra a tablazat 2. sorat
+console.log("Egyiptom | mezőgazdaság | csatornák | gátak |") // Kiirom konzolra a tablazat 3. sorat
+console.log("Spárta | neveléstudomány | agogé |  |") // Kiirom konzolra a tablazat 4. sorat
+console.log("Spárta | harcászat | hoplita | phalanx |") // Kiirom konzolra a tablazat 5. sorat
