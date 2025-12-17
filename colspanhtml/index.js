a5dbe84af3294d66d2fa5c9ac0be68894da712c7
gomszab <gombosszabolcs88@gmail.com>
2025-12-17 21:55:53 +0100

    col html esemeny fuggveny bev

--- colspanhtml/index.js
+++ colspanhtml/index.js
@@ -7,7 +7,6 @@
  */
 const simpleButton = document.getElementById('simplebutton') // elkerem a szimplasort hozzaado gombot
 simpleButton.addEventListener('click', function(){ // regisztralok egy esemenykezelot a szimplasor gomb click esemenyere
-    console.log('szimpla sor kattintas') // kiiratok valamit a console-ra esemeny eseten, hogy lassam lefut-e az esemenykezelo
 
     /**
      * @type {DataType} tartalmaz egy teszt adatot, amit hozzaadunk a tombhoz
@@ -23,32 +22,7 @@ simpleButton.addEventListener('click', function(){ // regisztralok egy esemenyke
      */
     const tbody = document.getElementById('tablebody') // elkerem a tablazat torzset
 
-    /**
-     * @type {HTMLDivElement} a tablazatunk uj sora
-     */
-    const row = document.createElement('div') // letrehozok egy divet a sornak
-    tbody.appendChild(row); // hozzacsatolom a tablazat torzsehez
-
-    /**
-     * @type {HTMLSpanElement} a tablazatunk uj soranak elso cellaja
-     */
-    const cell1 = document.createElement('span'); // letrehozok egy span-t az elso cellanak
-    row.appendChild(cell1); // hozzacsatolom a sor divjehez
-    cell1.innerText = newRow.where; // beallitom tartalomnak a newRow where tulajdonsaganak erteket
-
-    /**
-     * @type {HTMLSpanElement} a tablazatunk uj soranak masodik cellaja
-     */
-    const cell2 = document.createElement('span'); // letrehozok egy span-t az masodik cellanak
-    row.appendChild(cell2); // hozzacsatolom a sor divjehez
-    cell2.innerText = newRow.what; // beallitom tartalomnak a newRow  what tulajdonsaganak erteket
-
-    /**
-     * @type {HTMLSpanElement} a tablazatunk uj soranak harmadik cellaja
-     */
-    const cell3 = document.createElement('span'); // letrehozok egy span-t a harmadik cellanak
-    row.appendChild(cell3); // hozzacsatolom a sor divjehez
-    cell3.innerText = newRow.example1; // beallitom tartalomnak a newRow example1 tulajdonsaganak erteket
+    addNewRow(newRow, tbody); // meghivom az uj sor hozzafuzest
 
 })
 
@@ -73,11 +47,25 @@ doubleButton.addEventListener('click', function(){ // regisztralok egy esemenyke
      */
     const tbody = document.getElementById('tablebody') // elkerem a tablazat torzset
 
+    addNewRow(newRow, tbody); // meghivom az uj sor hozzafuzest
+
+})
+
+/**
+ * 
+ * Hozzafuz egy sort a tablazat torzsehez
+ * 
+ * @param {DataType} newRow az uj hozzaadando objektum
+ * @param {HTMLDivElement} tableBody a tablazattorzs ahova hozzafuzzuk az uj sort
+ * 
+ * @returns {void}
+ */
+function addNewRow(newRow, tableBody){ // definialunk egy fuggvenyt ket parameterrel
     /**
      * @type {HTMLDivElement} a tablazatunk uj sora
      */
     const row = document.createElement('div') // letrehozok egy divet a sornak
-    tbody.appendChild(row); // hozzacsatolom a tablazat torzsehez
+    tableBody.appendChild(row); // hozzacsatolom a tablazat torzsehez
 
     /**
      * @type {HTMLSpanElement} a tablazatunk uj soranak elso cellaja
@@ -100,11 +88,15 @@ doubleButton.addEventListener('click', function(){ // regisztralok egy esemenyke
     row.appendChild(cell3); // hozzacsatolom a sor divjehez
     cell3.innerText = newRow.example1; // beallitom tartalomnak a newRow example1 tulajdonsaganak erteket
 
-    /**
-     * @type {HTMLSpanElement} a tablazatunk uj soranak negyedik cellaja
-     */
-    const cell4 = document.createElement('span'); // letrehozok egy span-t a harmadik cellanak
-    row.appendChild(cell4); // hozzacsatolom a sor divjehez
-    cell4.innerText = newRow.example2; // beallitom tartalomnak a newRow example2 tulajdonsaganak erteket
-
-})
+    if(newRow.example2){ // vizsgaljuk, hogy az example2 tulajdonsag definialva van-e
+        /**
+         * @type {HTMLSpanElement} a tablazatunk uj soranak negyedik cellaja
+         */
+        const cell4 = document.createElement('span'); // letrehozok egy span-t a harmadik cellanak
+        row.appendChild(cell4); // hozzacsatolom a sor divjehez
+        cell4.innerText = newRow.example2; // beallitom tartalomnak a newRow example2 tulajdonsaganak erteket
+    }else{ //egyebkent
+        // colspan allitas helye
+    }
+    
+}
